1de86ea3adb61811b18f1d1671e46719cb40e810
cagatayok <114108871+cagatayok@users.noreply.github.com>
2024-02-08 18:05:02 +0300

    MaskeTakip

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Person person=new Person();
+            person.FirstName = "Çağatay";
+            person.LastName = "Ok";
+            person.DateOfBirthDay = 2004;
+            person.NationalIdentitiy = 123;
+
+            PttManager pttManager =new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+
+
+        }
+    }
+
+    
+}
